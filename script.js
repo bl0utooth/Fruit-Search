587b22180b5361211fcc15d166baf1696231ea69
bl0utooth <136910180+bl0utooth@users.noreply.github.com>
2023-12-28 16:18:11 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const input = document.querySelector('#fruit');
+const suggestions = document.querySelector('.suggestions ul');
+
+const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+
+function search(str) {
+	let results = [];
+
+
+    results = fruit.filter(f => f.toLowerCase().includes(str.toLowerCase()));
+
+	return results;
+};
+
+function searchHandler(e) {
+	const inputValue = e.target.value.trim();
+    const results = search(inputValue);
+    showSuggestions(results, inputValue);
+};
+
+function showSuggestions(results, inputVal) {
+    suggestions.innerHTML = '';
+    
+    if (inputVal !== '' && results.length > 0) {
+        results.forEach(result => {
+            const listItem = document.createElement('li');
+            listItem.textContent = result;
+            suggestions.appendChild(listItem);
+        });
+    }
+}
+
+function useSuggestion(e) {
+	if (e.target.tagName === 'LI') {
+        input.value = e.target.textContent;
+        suggestions.innerHTML = '';
+    }
+}
+
+input.addEventListener('keyup', searchHandler);
+suggestions.addEventListener('click', useSuggestion);
